28baaa37cbc09827e0ed1a08f2c0308831fedfb5
Tangy-Web <briantang05@gmail.com>
2025-04-12 19:50:36 -0500

    Added doodleImage on page

--- src/pages/DoodlePage.jsx
+++ src/pages/DoodlePage.jsx
@@ -1,7 +1,32 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Image } from "@chakra-ui/react";
+import {useDb} from "../providers/DatabaseProvider.jsx";
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function DoodlePage() {
-  return <Text>doodle page</Text>;
+  const [doodle, setDoodle] = useState(null);
+  const db = useDb();
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  useEffect(() => {
+    if (!id) {
+      return navigate("/doodles");
+    }
+    db.getDoodle(id).then((doodle) => {
+      if (!doodle) {
+        return navigate("/doodles");
+      }
+      setDoodle(doodle);
+    })
+  })
+
+  if (!doodle) {
+    return null
+  }
+  return (
+      <Image w = {500} h = {500} src = {doodle.data} className= "border" />
+  );
 }
 
 export default DoodlePage;
